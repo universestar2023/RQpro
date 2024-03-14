7a681410e44b0960d762acf23033aaa66c3e4547
universestar2023 <marufamansuri9@gmail.com>
2024-03-15 00:20:34 +0530

    third commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/header/navbar';
 import Section from './components/section1';
+import Section2 from './components/section2';
 
 function App() {
   return (
   <>
    <Navbar/>
     <Section/>
+    <Section2/>
   </>
   );
 }
