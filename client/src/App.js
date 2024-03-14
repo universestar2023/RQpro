df7b6254fa5aa2dbb720d611356fc878312eb01d
Sakshi Choudhary <sakshichoudhary6677@gmail.com>
2024-03-14 21:31:58 +0530

    free sign up added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/header/navbar';
 import Section from './components/section1';
+import FreeSignUp from './components/freeSignUp';
 
 function App() {
   return (
   <>
    <Navbar/>
     <Section/>
+    <FreeSignUp/>
   </>
   );
 }
