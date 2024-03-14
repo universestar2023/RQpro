3c88441f3cc819d4726d03d221667f7f64820f25
universestar2023 <marufamansuri9@gmail.com>
2024-03-15 00:39:41 +0530

    fifth

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
 import './App.css';
 import Navbar from './components/header/navbar';
 import Section from './components/section1';
-<<<<<<< HEAD
 import Section2 from './components/section2';
-=======
 import AssessmentSection from './components/AssessmentSection';
 import ComponentLayout from './components/ComponentLayout/ComponentLayout';
->>>>>>> 031b4c80bd6f181dc58ff6e7008920a314100949
 
 function App() {
   return (
   <>
    <Navbar/>
     <Section/>
-<<<<<<< HEAD
     <Section2/>
-=======
     <AssessmentSection /> 
     <ComponentLayout/>
->>>>>>> 031b4c80bd6f181dc58ff6e7008920a314100949
   </>
   );
 }
