73d9b771712ca6d83c43b6cf3da06687937a33f6
universestar2023 <marufamansuri9@gmail.com>
2024-03-15 00:39:41 +0530

    fifth

--- client/src/components/ComponentLayout/ComponentLayout.js
+++ client/src/components/ComponentLayout/ComponentLayout.js
@@ -1,28 +1,48 @@
-import React from 'react';
-import './ComponentLayout.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUsers, faComments } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import "./ComponentLayout.css";
 
 const ComponentLayout = () => {
-    return (
-        <div className="container">
-            <h1 className="heading">Unlock Your Legal Knowledge</h1>
-            <p className="text">Connect with our experts to enhance your legal literacy journey.</p>
-            
-            <div className="icons">
-                <div className="icon">
-                    <FontAwesomeIcon icon={faUsers} className="icon-img" />
-                    <h2 className="icon-heading">Community Support</h2>
-                </div>
-                <div className="icon">
-                    <FontAwesomeIcon icon={faComments} className="icon-img" />
-                    <h2 className="icon-heading">Expert Talk</h2>
-                </div>
-            </div>
-            
-            <a href="#" className="button">Sign Up Now</a>
+  return (
+    <div className="container">
+      <h1 className="heading">Unlock Your Legal Knowledge</h1>
+      <p className="text">
+        Connect with our experts to enhance your legal literacy journey.
+      </p>
+
+      <div className="icons">
+        <div className="icon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="50"
+            height="50"
+            fill="currentColor"
+            class="bi bi-chat-right-text-fill"
+            viewBox="0 0 16 16"
+          >
+            <path d="M16 2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h9.586a1 1 0 0 1 .707.293l2.853 2.853a.5.5 0 0 0 .854-.353zM3.5 3h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1 0-1m0 2.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1 0-1m0 2.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1" />
+          </svg>
+          <h2 className="icon-heading">Community Support</h2>
         </div>
-    );
+        <div className="icon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="50"
+            height="50"
+            fill="currentColor"
+            class="bi bi-people-fill"
+            viewBox="0 0 16 16"
+          >
+            <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5" />
+          </svg>
+          <h2 className="icon-heading">Expert Talk</h2>
+        </div>
+      </div>
+
+      <a href="#" className="button">
+        Sign Up Now
+      </a>
+    </div>
+  );
 };
 
 export default ComponentLayout;
