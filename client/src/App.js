993d60561ee6d5965e8323f70a87ced379f01de5
Sujata-Randhaye <sujatarandhaye@gmail.com>
2024-03-14 23:47:10 +0530

    added assessment feature and community experttalk

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Navbar from './components/header/navbar';
 import Section from './components/section1';
+import AssessmentSection from './components/AssessmentSection';
+import ComponentLayout from './components/ComponentLayout/ComponentLayout';
 
 function App() {
   return (
   <>
    <Navbar/>
     <Section/>
+    <AssessmentSection /> 
+    <ComponentLayout/>
   </>
   );
 }
