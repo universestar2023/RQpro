5a4f140b99a7b507af4f34913c34887721552925
Sujata-Randhaye <sujatarandhaye@gmail.com>
2024-03-18 19:50:55 +0530

    changed login page

--- client/src/components/Loginpage/login.js
+++ client/src/components/Loginpage/login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from '../LandingPage/header/navbar';
+import image from './image.jpg';
 import './login.css';
 class Login extends React.Component {
   constructor(props) {
