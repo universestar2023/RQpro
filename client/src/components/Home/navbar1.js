bfa53f4c6e3e79ef8f56eb3646493c7a8beaad28
Sakshi Choudhary <sakshichoudhary6677@gmail.com>
2024-03-24 10:25:16 +0530

    contact icon linked

--- client/src/components/Home/navbar1.js
+++ client/src/components/Home/navbar1.js
@@ -109,6 +109,7 @@ class Navbar1 extends React.Component {
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">
+              <Link to='/contact' >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="19"
@@ -119,7 +120,7 @@ class Navbar1 extends React.Component {
                 >
                   <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5m.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1z" />
                 </svg>
-                <Link to='/contact' >Contact</Link>
+                Contact</Link>
               </a>
             </li>
             <li className="nav-item">
