d7f8c02572dc5a3af11de06ea3938c79f3747565
Sujata-Randhaye <sujatarandhaye@gmail.com>
2024-04-21 09:23:48 +0530

    minor changes

--- client/src/Test/Questions.js
+++ client/src/Test/Questions.js
@@ -28,7 +28,7 @@ export default function Questions({ onChecked, questionNumber }) {
     dispatch(updateResult({ trace, checked }));
   }
 
-  if (isLoading) return <h3>Loading...</h3>;
+  if (isLoading) return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><circle fill="#FF156D" stroke="#FF156D" stroke-width="15" r="15" cx="40" cy="65"><animate attributeName="cy" calcMode="spline" dur="2" values="65;135;65;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.4"></animate></circle><circle fill="#FF156D" stroke="#FF156D" stroke-width="15" r="15" cx="100" cy="65"><animate attributeName="cy" calcMode="spline" dur="2" values="65;135;65;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.2"></animate></circle><circle fill="#FF156D" stroke="#FF156D" stroke-width="15" r="15" cx="160" cy="65"><animate attributeName="cy" calcMode="spline" dur="2" values="65;135;65;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="0"></animate></circle></svg>;
   if (serverError) {
     // If serverError is an object, extract the message property
     const errorMessage = typeof serverError === 'object' ? serverError.message : serverError;
