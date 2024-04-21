32fc5ce68a45e74302fcd95199497273d9c2e020
universestar2023 <marufamansuri9@gmail.com>
2024-04-21 20:49:13 +0530

    c9

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ connection();
 
 // middlewares
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "https://rights.vercel.app/" }));
 config();
 
 // routes
